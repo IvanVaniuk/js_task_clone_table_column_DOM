e41f1a873cee3e0c3ab41b870cb58e060fdff65e
Ivan Vaniuk <ivanvanuk77@gmail.com>
2025-04-28 19:40:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
+const rows = table.rows;
+const tbodyRows = tbody.rows;
+const copy = [];
+
+function createTableCell(content, isHeader = false) {
+  const newCell = document.createElement(isHeader ? 'th' : 'td');
+
+  newCell.textContent = content;
+
+  return newCell;
+}
+
+function insertCellBeforeLast(row, cell) {
+  row.insertBefore(cell, row.cells[row.cells.length - 1]);
+}
+
+for (const elem of tbodyRows) {
+  copy.push(elem.cells[1].textContent);
+}
+
+for (let i = 0; i < rows.length; i++) {
+  const elem = rows[i];
+  const isHeader = i === 0 || i === rows.length - 1;
+  const newCell = createTableCell(elem.cells[1].textContent, isHeader);
+
+  insertCellBeforeLast(elem, newCell);
+}
